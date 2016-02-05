c5da1fdf9a26279c2c803e2aa21b5d4f6e3abb4d
Rik ter Beek <rikterbeek@users.noreply.github.com>
2016-02-05 13:29:04 +0100

    Do not send sensitive card data to server

--- app/code/Adyen/Payment/view/frontend/web/js/view/payment/method-renderer/adyen-cc-method.js
+++ app/code/Adyen/Payment/view/frontend/web/js/view/payment/method-renderer/adyen-cc-method.js
@@ -94,14 +94,7 @@ define(
                 return {
                     'method': this.item.method,
                     'cc_type': this.creditCardType(),
-                    'cc_exp_year': this.creditCardExpYear(),
-                    'cc_exp_month': this.creditCardExpMonth(),
-                    'cc_number': this.creditCardNumber(),
-                    'cc_owner' : this.creditCardOwner(),
                     additional_data: {
-                        'cc_cid': this.creditCardVerificationNumber(),
-                        'cc_ss_start_month': this.creditCardSsStartMonth(),
-                        'cc_ss_start_year': this.creditCardSsStartYear(),
                         'encrypted_data': this.encryptedData()
                     }
                 };
